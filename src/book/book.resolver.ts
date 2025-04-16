83c384111839ff2cb9af1be7505ecf1273d8326b
SATYAM-PRATIBHAN <satyampratibhan@gmail.com>
2025-04-17 01:11:43 +0530

    feat : added types

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,15 +1,18 @@
 import { Query, Resolver } from "@nestjs/graphql";
 import { Book } from "./book.schema";
+import { Book as BookModel } from "src/graphql";
 
 @Resolver(of => Book)
 export class BookResolver { 
     @Query(returns => [Book], {name : 'books'})
     getAll() {
-        return [
+        let arr : BookModel[] = [
             {id : 1, title : 'book1', price : 1000},
             {id : 2, title : 'book2', price : 2000},
-            {id : 3, title : 'book3', price : 3000}, 
+            {id : 3, title : 'book3', price : 3000},
         ]
+
+        return arr;
     }
 
     @Query(returns => Book, {name : 'book'})
