9f3a939f7494e60fa35fdcb826abc5f07a01b905
SATYAM-PRATIBHAN <satyampratibhan@gmail.com>
2025-04-17 23:46:57 +0530

    feat : dummy db representation

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -4,4 +4,33 @@ import { BookEntity } from "./entity/book.entity";
 @Injectable()
 export class BookService {
     public booksData : BookEntity[] = []
+
+    addBook(book : BookEntity) : string {
+        this.booksData.push(book)
+        return "Book added successfully"
+    }
+
+    updateBook(id : number, updateBook : BookEntity) : string{
+        for(let i = 0; i < this.booksData.length; i++) {
+            if(this.booksData[i].id === id) this.booksData[i] = updateBook; 
+        }
+        return "Book updated successfully"
+    }
+
+    deleteBook(id : number) : string {
+        this.booksData = this.booksData.filter(book => book.id != id)
+        return "Book deleted successfully"
+    }
+
+    getBook(id : number) : BookEntity {
+        const book = this.booksData.find(book => book.id === id)
+        if(!book) {
+            throw new Error("Book not found")
+        }
+        return book
+    }
+
+    getBooks() : BookEntity[] {
+        return this.booksData;
+    }
 }
