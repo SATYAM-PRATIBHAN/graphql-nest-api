8421b558a5f6e8c254decf8a31a7a4b73866c18b
SATYAM-PRATIBHAN <satyampratibhan@gmail.com>
2025-04-18 16:15:50 +0530

    feat : Added some more data functions

--- src/graphql.ts
+++ src/graphql.ts
@@ -11,12 +11,16 @@
 export interface Book {
     id: number;
     title: string;
-    price?: Nullable<number>;
+    price: number;
 }
 
 export interface IQuery {
-    books(): Book[] | Promise<Book[]>;
-    book(): Book | Promise<Book>;
+    index(): string | Promise<string>;
+    getAllBooks(): Book[] | Promise<Book[]>;
+    getBookById(): Book | Promise<Book>;
+    addBook(): string | Promise<string>;
+    deleteBook(): string | Promise<string>;
+    updateBook(): string | Promise<string>;
 }
 
 type Nullable<T> = T | null;
